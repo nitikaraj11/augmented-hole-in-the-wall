a841869700a7c30d2cd77668a3e9296576158d7c
Nitika <nitikarj@In-Nitikarj-4.local>
2020-01-12 00:11:28 +0530

    Add Controller to visualise the scanned image | Nitika

--- Assets/GoogleARCore/Examples/AugmentedImage/Scripts/AugmentedImageVisualizer.cs
+++ Assets/GoogleARCore/Examples/AugmentedImage/Scripts/AugmentedImageVisualizer.cs
@@ -32,30 +32,8 @@ namespace GoogleARCore.Examples.AugmentedImage
     /// </summary>
     public class AugmentedImageVisualizer : MonoBehaviour
     {
-        /// <summary>
-        /// The AugmentedImage to visualize.
-        /// </summary>
         public AugmentedImage Image;
-
-        /// <summary>
-        /// A model for the lower left corner of the frame to place when an image is detected.
-        /// </summary>
-        public GameObject FrameLowerLeft;
-
-        /// <summary>
-        /// A model for the lower right corner of the frame to place when an image is detected.
-        /// </summary>
-        public GameObject FrameLowerRight;
-
-        /// <summary>
-        /// A model for the upper left corner of the frame to place when an image is detected.
-        /// </summary>
-        public GameObject FrameUpperLeft;
-
-        /// <summary>
-        /// A model for the upper right corner of the frame to place when an image is detected.
-        /// </summary>
-        public GameObject FrameUpperRight;
+        public GameObject Center;
 
         /// <summary>
         /// The Unity Update method.
@@ -64,28 +42,10 @@ namespace GoogleARCore.Examples.AugmentedImage
         {
             if (Image == null || Image.TrackingState != TrackingState.Tracking)
             {
-                FrameLowerLeft.SetActive(false);
-                FrameLowerRight.SetActive(false);
-                FrameUpperLeft.SetActive(false);
-                FrameUpperRight.SetActive(false);
+                Center.SetActive(false);
                 return;
             }
-
-            float halfWidth = Image.ExtentX / 2;
-            float halfHeight = Image.ExtentZ / 2;
-            FrameLowerLeft.transform.localPosition =
-                (halfWidth * Vector3.left) + (halfHeight * Vector3.back);
-            FrameLowerRight.transform.localPosition =
-                (halfWidth * Vector3.right) + (halfHeight * Vector3.back);
-            FrameUpperLeft.transform.localPosition =
-                (halfWidth * Vector3.left) + (halfHeight * Vector3.forward);
-            FrameUpperRight.transform.localPosition =
-                (halfWidth * Vector3.right) + (halfHeight * Vector3.forward);
-
-            FrameLowerLeft.SetActive(true);
-            FrameLowerRight.SetActive(true);
-            FrameUpperLeft.SetActive(true);
-            FrameUpperRight.SetActive(true);
+            Center.SetActive(true);
         }
     }
 }
