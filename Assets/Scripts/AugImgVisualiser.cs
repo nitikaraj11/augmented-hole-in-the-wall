29ef388f8265cba434e0dcd770612c2689e49f53
Nitika <nitikarj@In-Nitikarj-4.local>
2020-01-11 23:04:57 +0530

    Add Augmented Image Visualiser | Nitika

--- Assets/Scripts/AugImgVisualiser.cs
+++ Assets/Scripts/AugImgVisualiser.cs
@@ -1,12 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using GoogleARCore;
+
 
 public class AugImgVisualiser : MonoBehaviour
 {
+     public AugmentedImage ImageToBeVisualised;
+    public GameObject Center;
+
     // Update is called once per frame
     void Update()
     {
-        
+        if (ImageToBeVisualised == null || ImageToBeVisualised.TrackingState != TrackingState.Tracking)
+        {
+            Center.SetActive(false);
+            return;
+        }
+        var centerText = Center.transform.GetChild(0).gameObject;
+        centerText.GetComponent<TMPro.TextMeshPro>().text = "Sanned & Identified";
     }
 }
