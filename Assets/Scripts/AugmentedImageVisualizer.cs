7c5910f8e78fb8eaa512ee2ec1e5becfe58fe1de
Nitika <nitikarj@In-Nitikarj-4.local>
2020-01-15 19:01:33 +0530

    Add new Controller script | Nitika

---
+++ Assets/Scripts/AugmentedImageVisualizer.cs
@@ -0,0 +1,42 @@
+﻿
+    using System;
+    using System.Collections.Generic;
+    using System.Runtime.InteropServices;
+    using GoogleARCore;
+    using GoogleARCoreInternal;
+    using UnityEngine;
+
+    /// <summary>
+    /// Uses 4 frame corner objects to visualize an AugmentedImage.
+    /// </summary>
+    public class AugmentedImageVisualizer : MonoBehaviour
+    {
+        /// <summary>
+        /// The AugmentedImage to visualize.
+        /// </summary>
+        public AugmentedImage Image;
+
+        /// <summary>
+        /// A model for the center of the frame to place when an image is detected.
+        /// </summary>
+        public GameObject Center;
+
+
+        /// <summary>
+        /// The Unity Update method.
+        /// </summary>
+        public void Update()
+        {
+            if (Image == null || Image.TrackingState != TrackingState.Tracking)
+            {
+                Center.SetActive(false);
+                return;
+            }
+
+            var centerText = Center.transform.GetChild(0).gameObject;
+            centerText.GetComponent<TMPro.TextMeshPro>().text = "Identified";
+
+            Center.SetActive(true);
+        }
+    }
+
